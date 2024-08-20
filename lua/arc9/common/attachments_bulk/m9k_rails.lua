8cc619ed3866fa42e58b18e04a61924a2a896fc7
NotTrashraD <bellie1579@gmail.com>
2024-08-20 23:19:56 +0200

    mp5

--- lua/arc9/common/attachments_bulk/m9k_rails.lua
+++ lua/arc9/common/attachments_bulk/m9k_rails.lua
@@ -29,3 +29,36 @@ ATT.Attachments = {
 
 ARC9.LoadAttachment(ATT, "r_m9k_dovetail")
 
+
+
+
+///////////////////////////////////////      r_mount_mp5_railb
+
+
+ATT = {}
+
+ATT.PrintName = "MP5 Rail Mount"
+ATT.Description = [[A rail mount for mounting optics on the MP5.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "reshed"
+ATT.Free = false
+
+ATT.Category = {"m9k_mp5"}
+ATT.Model = "models/shared/mounts/shaerd_mount_mp5.mdl"
+ATT.Scale = 1
+ATT.ModelOffset = Vector(0, 0, 0)
+
+ATT.Attachments = {
+    {
+        PrintName = "Optic",
+        Pos = Vector(0, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 0, 1.5),
+        Category = {"m9k_opticmedium"},
+    }
+}
+
+
+ARC9.LoadAttachment(ATT, "r_mount_mp5_railb")
+
+
