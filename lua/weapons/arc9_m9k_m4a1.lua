918099ceecd2d1f9e73968e02081b106a20cb376
NotTrashraD <bellie1579@gmail.com>
2024-08-22 02:57:45 +0200

    Update arc9_m9k_m4a1.lua

--- lua/weapons/arc9_m9k_m4a1.lua
+++ lua/weapons/arc9_m9k_m4a1.lua
@@ -180,10 +180,10 @@ SWEP.RecoilLookupTable = nil -- Use to set specific values for predictible recoi
 SWEP.RecoilLookupTableOverrun = nil -- Repeatedly take values from this table if we run out in the main table
 
 -- General recoil multiplier
-SWEP.Recoil = 1.6
+SWEP.Recoil = 1.45
 
 -- These multipliers affect the predictible recoil by making the pattern taller, shorter, wider, or thinner.
-SWEP.RecoilUp = 0.85 -- Multiplier for vertical recoil
+SWEP.RecoilUp = 0.80 -- Multiplier for vertical recoil
 SWEP.RecoilSide = 0.1 -- Multiplier for vertical recoil
 
 -- These values determine how much extra movement is applied to the recoil entirely randomly, like in a circle.
